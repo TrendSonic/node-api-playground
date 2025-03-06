db67f6c9b98d1469dd3e9d0111d2939ce2a7b126
unknown <edinosmic77@gmail.com>
2025-03-06 15:17:11 +0100

    routes/bootcamp.js

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const dotenv = require("dotenv");
 
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+  console.log(
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+  );
 });
