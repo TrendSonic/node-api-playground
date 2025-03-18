6dae5b62da53796dc04275a9b69fb2099a553d19
unknown <edinosmic77@gmail.com>
2025-03-18 12:37:30 +0100

    colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
 
@@ -24,12 +25,13 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(
-    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode (✓)`
+      .yellow
   );
 });
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message} (✗)`.red);
 
   server.close(() => process.exit(1));
 });
